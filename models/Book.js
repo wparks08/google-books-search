fc3587fb653b3374a6f8427dea76df05412351c0
Will Parks <wparks08@gmail.com>
2020-02-18 18:26:28 -0800

    book schema added

---
+++ models/Book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: String,
+    authors: Array,
+    description: String,
+    image: String,
+    link: String
+});
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googleBooksSearch";
+
 const app = express();
+mongoose.connect(MONGODB_URI);
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 // Define API routes here
@@ -16,9 +21,9 @@ if (process.env.NODE_ENV === "production") {
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
